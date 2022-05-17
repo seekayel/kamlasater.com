171a5a56f42575bbab63bce175cae78618c13de9
Kam Lasater <ckl@seekayel.com>
2022-05-16 21:58:30 -0400

    Added mdsvex

--- svelte.config.js
+++ svelte.config.js
@@ -1,13 +1,18 @@
 import adapter from '@sveltejs/adapter-node';
 import sveltePreprocess from 'svelte-preprocess'
+import { mdsvex } from 'mdsvex'
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
+  extensions: ['.svelte', '.md'],
 	kit: {
 		adapter: adapter({ out: 'dist' })
 	},
   preprocess: [
-    sveltePreprocess(),
+    mdsvex({
+      extensions: ['.md']
+    }),
+    sveltePreprocess()
   ],
 };
 
