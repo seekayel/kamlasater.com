23380d0aa8ece670d7a55083a3e23ff2916d8689
Kam Lasater <ckl@seekayel.com>
2022-06-09 10:13:18 -0400

    Added header left-right

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,57 +1,73 @@
 <header>
-    <a href="/">Home</a>
-    <a href="/blog">Blog</a>
-
-    <nav>
-      <ul>
-        <li>
-          <a target="_blank" href="https://github.com/seekayel">
-            <i class="fa-brands fa-github" title="Github link"></i>
-          </a>
-        </li><li>
-          <a target="_blank" href="https://www.linkedin.com/in/kamlasater">
-            <i class="fa-brands fa-linkedin" title="Linkedin link"></i>
-          </a>
-        </li><li>
-          <a target="_blank" href="https://twitter.com/seekayel">
-            <i class="fa-brands fa-twitter" title="Twitter link"></i>
-          </a>
-        </li>
-        <!-- <li>
-          <a href="mailto:ckl@seekayel.com" target="_blank">ckl@seekayel.com</a>
-        </li>
-        <li>
-          <a href="/uses">Built With</a>
-        </li> -->
-        <!-- <li>
-          <a href="/about">About</a>
-        </li>
-        <li>
-          <a href="/contact">Contact</a>
-        </li> -->
-      </ul>
-    </nav>
+  <nav class="menu">
+    <ul class="menu-left">
+      <li>
+        <a href="/">Home</a>
+      </li>
+      <li>
+        <a href="/blog">Blog</a>
+      </li>
+    </ul>
+    <ul class="menu-right">
+      <li>
+        <a target="_blank" href="https://github.com/seekayel">
+          <i class="fa-brands fa-github" title="Github link"></i>
+        </a>
+      </li><li>
+        <a target="_blank" href="https://www.linkedin.com/in/kamlasater">
+          <i class="fa-brands fa-linkedin" title="Linkedin link"></i>
+        </a>
+      </li><li>
+        <a target="_blank" href="https://twitter.com/seekayel">
+          <i class="fa-brands fa-twitter" title="Twitter link"></i>
+        </a>
+      </li>
+      <!-- <li>
+        <a href="mailto:ckl@seekayel.com" target="_blank">ckl@seekayel.com</a>
+      </li>
+      <li>
+        <a href="/uses">Built With</a>
+      </li> -->
+      <!-- <li>
+        <a href="/about">About</a>
+      </li>
+      <li>
+        <a href="/contact">Contact</a>
+      </li> -->
+    </ul>
+  </nav>
 </header>
 
 <style>
-    header {
-        padding: 1rem;
-        background: darkblue;
-        color: white;
-        display: flex;
-        flex-wrap: wrap;
-        justify-content: space-between;
-    }
+  header {
+      padding: 1rem;
+      background: darkblue;
+      color: white;
+  }
 
-    ul {
-        margin: 0;
-        list-style-type: none;
-        display: flex;
-        gap: 1rem;
-    }
+  nav.menu{
+    display: flex;
+    justify-content: space-between;
+  }
+  nav.menu ul{
+      margin: 0;
+      padding: 0;
+      list-style: none;
+  }
+  nav.menu ul li{
+      display: inline-block;
+      gap: 1rem;
+      padding-left: 1em;
+  }
 
-    a {
-        text-decoration: none;
-        color: inherit;
-    }
+  /* ul {
+      margin: 0;
+      list-style-type: none;
+      display: flex;
+  } */
+
+  a {
+      text-decoration: none;
+      color: inherit;
+  }
 </style>
