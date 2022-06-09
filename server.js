581af53b72885f30869b2a1e6ae72599daab920f
Kam Lasater <ckl@seekayel.com>
2022-06-09 10:32:04 -0400

    Added custom server to set ico content-types

---
+++ server.js
@@ -0,0 +1,27 @@
+import { handler } from './dist/handler.js';
+import express from 'express';
+
+const app = express();
+
+// add a route that lives separately from the SvelteKit app
+app.get('/healthcheck', (req, res) => {
+  res.end('ok');
+});
+
+// handle static folder ourselves which allows us to set custom headers
+app.use(express.static(
+	'static',
+	{
+		setHeaders: (res, pathname, stats) => {
+			if (pathname.match(/\.ico$/))
+				res.setHeader('content-type', 'image/x-icon');
+		}
+	}
+));
+
+// let SvelteKit handle everything else, including serving prerendered pages and static assets
+app.use(handler);
+
+app.listen(3000, () => {
+	console.log('listening on port 3000');
+});
