e52ee5f2f22571a839fb7af2bb2a9d86bb9fa080
Kam Lasater <ckl@seekayel.com>
2022-06-09 09:51:13 -0400

    Added json api endpoint

--- src/routes/api/posts.json.js
+++ src/routes/api/posts.json.js
@@ -1,7 +1,23 @@
 // posts.json.js
-export const get = () => {
+export const get = async () => {
+
+  const allPostFiles = import.meta.glob('../blog/*.md')
+  const iterPostFiles = Object.entries(allPostFiles)
+
+  const allPosts = await Promise.all(
+    iterPostFiles.map(async ([path, resolver]) => {
+      const {metadata} = await resolver()
+      const post = path.slice(2, -3)
+      return {metadata, post}
+    })
+  )
+
+  const sortedPosts = allPosts.sort((a,b) => {
+    return new Date(b.metadata.date) - new Date(a.metadata.date)
+  })
+
   return {
     status: 200,
-    body: "Welcome to my API"
+    body: sortedPosts
   }
 }
