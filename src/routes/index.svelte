4287b6b4af5524684d1f1f8af4d28c7e0dbf314c
Kam Lasater <ckl@seekayel.com>
2024-03-10 14:33:46 -0400

    Updated resume

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,206 +1,213 @@
-<body><div>
-  <div>
-    <div>
-      <h1>Kam Lasater</h1>
-      <h2>Software Entrepreneur</h2>
-    </div>
-  </div>
-</div>
+<body>
+
 <main class="page-content" aria-label="Content">
-    <div class="wrapper"><div class="container intro-container">
-  <h3>About Me</h3>
-  <div class="row clearfix"><div class="col-xs-12 col-sm-4 col-md-3 no-print">
+  <div>
+    <h1>Kam Lasater</h1>
+    <h3>Software Entrepreneur</h3>
+    <br>
+  </div>
+
+  <div class="wrapper"><div class="container intro-container">
+    <h2>About Me</h2>
         <!-- <span class="profile-img" style="background-image: url(/static/kam-lasater.png)"></span> -->
-      </div><div class="col-xs-12 col-sm-8 col-md-9 col-print-12">
-      <p>Motivated to build and grow things.</p>
+    <p>I build and grow things.</p>
 
-<p>In professional life that means software and software companies.</p>
+    <p>In professional life that means software and software companies. In personal
+      life that means I am a husband, father of four boys and gardener outside of
+      Philadelphia PA. Mornings you can find me running, lifting or rolling bjj.</p>
+    <br>
+  </div>
 
-<p>Currently focused on hatching my next business.</p>
 
-<p>Husband, father of three boys and gardener outside of Philadelphia PA.</p>
+  <div class="container experience-container">
+    <h2>Experience</h2>
+    <br>
+
+    <div>
+      <h3>Cyclic (<a href="//www.cyclic.sh" target="_blank" class="link">www.cyclic.sh</a>)</h3>
+      <p><b>CEO / Cofounder</b></p>
+      <p>January 2021 - present</p>
+      <ul>
+        <li>Full stack javascript/python apps</li>
+        <li>From idea to 150k+ developers</li>
+        <li>Fully AWS hosted PaaS built by team of 2</li>
+      </ul>
+    </div>
+    <br>
+
+    <div>
+      <h3>Black Knight Financial Services (<a href="//bkfs.com" target="_blank" class="link">bkfs.com</a>)</h3>
+      <p><b>Senior Development Manager</b></p>
+      <p>June 2018 - April 2020</p>
+      <ul>
+        <li>Exceeded organization sales targets for 2.5 yrs</li>
+        <li>Serverless AI product launched with 3 of top 5 US banks</li>
+        <li>Migrated to serverless build system</li>
+        <li>9 AWS Certifications in 14 months</li>
+        <li>Completed PCI SAQ-d process with assessor</li>
+        <li>Grew Engineering team from 6 to 19 engineers</li>
+        <li>Managed transition of Product POC to remote team of 8+ engineers</li>
+      </ul>
+    </div>
+    <br>
+
+    <div>
+      <h3>HeavyWater (<a href="//heavywater.com" target="_blank" class="link">heavywater.com</a>)</h3>
+      <p><b>Chief Technology Officer</b></p>
+      <p>January 2018 - June 2018</p>
+      <ul>
+        <li>Grew dev team from 3 to 6</li>
+        <li>Launched product</li>
+        <li>Managed AWS infrastructure and security</li>
+        <li><a href="https://technical.ly/philly/2018/06/05/heavywater-acquired-by-florida-based-ai/">Acquired by Black Knight</a>.</li>
+      </ul>
+    </div>
+    <br>
+
+
+
+
+
+
+      <div class="row clearfix layout layout-left">
+    <div class="col-xs-12 col-sm-4 col-md-3 col-print-12 details"><h4>LegalScience</h4>
+  <a href="https://legalscience.com" target="_blank" class="link">
+  https://legalscience.com</a><p><b>Chief Executive Officier</b></p><p>April 2017 - December 2018</p></div>
+    <div class="col-xs-12 col-sm-8 col-md-9 col-print-12"><ul>
+    <li>Closed first commercial data deal</li>
+    <li>Awarded 2+ million dollar SBIR grant</li>
+    <li>Implemented sales and marketing tracking tools</li>
+    <li>Implemented modern CICD practices</li>
+  </ul>
 
     </div>
   </div>
-</div>
-<div class="container projects-container">
-  <h3>Projects</h3>
-
-    <div class="row clearfix layout layout-top-middle">
-  <div class="col-xs-12 col-print-12 details"><h4>Quantified Software</h4>
-<a href="//quantifiedsoftware.com" target="_blank" class="link">
-quantifiedsoftware.com</a></div>
-  <div class="col-xs-12 col-print-12"><p>Exploring how to measure the software production process.</p>
-<ul>
-  <li><a href="https://www.youtube.com/channel/UCz-mJfAY8vjz9v4D8ePNj-w">Quantified Software on Youtube</a></li>
-  <li><a href="https://twitter.com/quantsoftware">Quantified Software on Twitter</a></li>
-</ul>
-
-  </div>
-</div>
 
 
-    <div class="row clearfix layout layout-top-middle">
-  <div class="col-xs-12 col-print-12 details"><h4>SeeClickFix</h4>
-<a href="//seeclickfix.com" target="_blank" class="link">
-seeclickfix.com</a></div>
-  <div class="col-xs-12 col-print-12"><p>Cofounder of community driven government engagement and 311 platform. Venture backed and acquired in 2019.</p>
-
-  </div>
-</div>
-
-
-    <div class="row clearfix layout layout-top-middle">
-  <div class="col-xs-12 col-print-12 details"><h4>MakeHaven</h4>
-<a href="https://www.makehaven.org/" target="_blank" class="link">
-https://www.makehaven.org/</a></div>
-  <div class="col-xs-12 col-print-12"><p>Cofounder of non-profit makerspace.</p>
-
-  </div>
-</div>
-
-
-    <div class="row clearfix layout layout-top-middle">
-  <div class="col-xs-12 col-print-12 details"><h4>New Haven Bank (nee Start Bank)</h4>
-<a href="https://www.newhavenbank.com/" target="_blank" class="link">
-https://www.newhavenbank.com/</a></div>
-  <div class="col-xs-12 col-print-12"><p>Board member of CDFI bank. Member of audit commitee.</p>
-
-  </div>
-</div>
-
-
-    <div class="row clearfix layout layout-top-middle">
-  <div class="col-xs-12 col-print-12 details"><h4>Piro Farm</h4>
-<a href="https://pirofarm-blog.tumblr.com/" target="_blank" class="link">
-https://pirofarm-blog.tumblr.com/</a></div>
-  <div class="col-xs-12 col-print-12"><p>Experiments in gardening and maker on activities.</p>
-
-  </div>
-</div>
-
-
-</div>
-<div class="container experience-container">
-  <h3>Experience</h3>
-
-    <div class="row clearfix layout layout-left">
-  <div class="col-xs-12 col-sm-4 col-md-3 col-print-12 details"><h4>Cyclic.sh</h4>
-<a href="//www.cyclic.sh" target="_blank" class="link">
-www.cyclic.sh</a><p><b>CEO and Cofounder</b></p><p>January 2021 - present</p></div>
-  <div class="col-xs-12 col-sm-8 col-md-9 col-print-12"><ul>
-  <li>Full stack javascript apps</li>
-  <li>From idea to … (in progress)</li>
-  <li>Come build with us</li>
-</ul>
-
-  </div>
-</div>
-
-
-    <div class="row clearfix layout layout-left">
-  <div class="col-xs-12 col-sm-4 col-md-3 col-print-12 details"><h4>Black Knight Financial Services</h4>
-<a href="//bkfs.com" target="_blank" class="link">
-bkfs.com</a><p><b>Senior Development Manager</b></p><p>June 2018 - April 2020</p></div>
-  <div class="col-xs-12 col-sm-8 col-md-9 col-print-12"><ul>
-  <li>Exceeded organization sales targets for 2.5 yrs</li>
-  <li>Serverless AI product launched with 3 of top 5 US banks</li>
-  <li>9 AWS Certifications in 14 months</li>
-  <li>Completed PCI SAQ-d process with assessor</li>
-  <li>Grew Engineering team from 6 to 19</li>
-</ul>
-
-  </div>
-</div>
-
-
-    <div class="row clearfix layout layout-left">
-  <div class="col-xs-12 col-sm-4 col-md-3 col-print-12 details"><h4>HeavyWater</h4>
-<a href="//heavywater.com" target="_blank" class="link">
-heavywater.com</a><p><b>Chief Technology Officier</b></p><p>January 2018 - June 2018</p></div>
-  <div class="col-xs-12 col-sm-8 col-md-9 col-print-12"><ul>
-  <li>Grew dev team from 3 to 6</li>
-  <li>Launched product</li>
-  <li><a href="https://technical.ly/philly/2018/06/05/heavywater-acquired-by-florida-based-ai/">Acquired by Black Knight</a>.</li>
-</ul>
-
-  </div>
-</div>
-
-
-    <div class="row clearfix layout layout-left">
-  <div class="col-xs-12 col-sm-4 col-md-3 col-print-12 details"><h4>LegalScience</h4>
-<a href="https://legalscience.com" target="_blank" class="link">
-https://legalscience.com</a><p><b>Chief Executive Officier</b></p><p>April 2017 - December 2018</p></div>
-  <div class="col-xs-12 col-sm-8 col-md-9 col-print-12"><ul>
-  <li>Closed first commercial data deal</li>
-  <li>Awarded 2+ million dollar SBIR grant</li>
-  <li>Implemented sales and marketing tracking tools</li>
-  <li>Implemented modern CICD practices</li>
-</ul>
-
-  </div>
-</div>
-
-
-    <div class="row clearfix layout layout-left">
-  <div class="col-xs-12 col-sm-4 col-md-3 col-print-12 details"><h4>SeeClickFix</h4>
-<a href="https://seeclickfix.com" target="_blank" class="link">
-https://seeclickfix.com</a><p><b>Cofounder</b></p><p>January 2008 - October 2019</p></div>
-  <div class="col-xs-12 col-sm-8 col-md-9 col-print-12"><ul>
-  <li>From idea to successful cash exit</li>
-  <li>Raised seed and two institutional rounds</li>
-  <li>Developed SaaS business model</li>
-</ul>
-
-  </div>
-</div>
-
-
-</div>
-<div class="container education-container">
-  <h3>Education</h3>
-
-    <div class="row clearfix layout layout-left">
-  <div class="col-xs-12 col-sm-4 col-md-3 col-print-12 details"><h4>Carnegie Mellon University</h4><p><b>BS Electrical and Computer Engineering</b></p><p>1999 - 2003</p></div>
-  <div class="col-xs-12 col-sm-8 col-md-9 col-print-12"><p>I focused on the interface between hardware and software.</p>
-<ul>
-  <li>Course work: Operating Systems, Embedded Systems, Advanced Digital Design and Advanced Prototyping Lab</li>
-  <li>Teaching Assistant: Embedded Systems and Advanced Digital Design</li>
-  <li>Languages: C, Verilog, Java</li>
-  <li>Tools: XEmacs, Make, CVS</li>
-</ul>
-
-  </div>
-</div>
-
-
-</div>
-<div class="container more-container">
-  <h3>Certifications</h3>
-  <div class="row clearfix">
-    <div class="col-md-12">
-      <p>Amazon Web Services</p>
-<ul>
-  <li><a href="https://www.certmetrics.com/amazon/public/badge.aspx?i=4&amp;t=c&amp;d=2019-12-20&amp;ci=AWS00532053">AWS Solutions Architect Professional</a></li>
-  <li>AWS Devops Professional</li>
-  <li>AWS Networking Specialist</li>
-  <li>AWS Security Specialist</li>
-  <li>AWS Big Data Specialist</li>
-  <li>AWS Machine Learning Specialist</li>
-  <li>AWS Solutions Architect Associate</li>
-  <li>AWS Sysops Associate</li>
-  <li>AWS Developer Associate</li>
-</ul>
+      <div class="row clearfix layout layout-left">
+    <div class="col-xs-12 col-sm-4 col-md-3 col-print-12 details"><h4>SeeClickFix</h4>
+  <a href="https://seeclickfix.com" target="_blank" class="link">
+  https://seeclickfix.com</a><p><b>Cofounder</b></p><p>January 2008 - October 2019</p></div>
+    <div class="col-xs-12 col-sm-8 col-md-9 col-print-12"><ul>
+    <li>From idea to successful cash exit</li>
+    <li>Raised seed and two institutional rounds</li>
+    <li>Developed SaaS business model</li>
+  </ul>
 
     </div>
   </div>
-</div>
-</div>
-  </main><div class="container footer-container">
-  <p>
-    Kam Lasater -
-    <a href="mailto:ckl@seekayel.com" target="_blank">ckl@seekayel.com</a></p>
-</div>
-<script src="/assets/js/index.js"></script>
+
+
+  </div>
+
+
+
+
+  <div class="container projects-container">
+    <h2>Projects</h2>
+
+    <div class="row clearfix layout layout-top-middle">
+      <div class="col-xs-12 col-print-12 details"><h4>Quantified Software</h4>
+    <a href="//quantifiedsoftware.com" target="_blank" class="link">
+    quantifiedsoftware.com</a></div>
+      <div class="col-xs-12 col-print-12"><p>Exploring how to measure the software production process.</p>
+    <ul>
+      <li><a href="https://www.youtube.com/channel/UCz-mJfAY8vjz9v4D8ePNj-w">Quantified Software on Youtube</a></li>
+      <li><a href="https://twitter.com/quantsoftware">Quantified Software on Twitter</a></li>
+    </ul>
+
+      </div>
+    </div>
+
+
+        <div class="row clearfix layout layout-top-middle">
+      <div class="col-xs-12 col-print-12 details"><h4>SeeClickFix</h4>
+    <a href="//seeclickfix.com" target="_blank" class="link">
+    seeclickfix.com</a></div>
+      <div class="col-xs-12 col-print-12"><p>Cofounder of community driven government engagement and 311 platform. Venture backed and acquired in 2019.</p>
+
+      </div>
+    </div>
+
+
+        <div class="row clearfix layout layout-top-middle">
+      <div class="col-xs-12 col-print-12 details"><h4>MakeHaven</h4>
+    <a href="https://www.makehaven.org/" target="_blank" class="link">
+    https://www.makehaven.org/</a></div>
+      <div class="col-xs-12 col-print-12"><p>Cofounder of non-profit makerspace.</p>
+
+      </div>
+    </div>
+
+
+        <div class="row clearfix layout layout-top-middle">
+      <div class="col-xs-12 col-print-12 details"><h4>New Haven Bank (nee Start Bank)</h4>
+    <a href="https://www.newhavenbank.com/" target="_blank" class="link">
+    https://www.newhavenbank.com/</a></div>
+      <div class="col-xs-12 col-print-12"><p>Board member of CDFI bank. Member of audit commitee.</p>
+
+      </div>
+    </div>
+
+
+        <div class="row clearfix layout layout-top-middle">
+      <div class="col-xs-12 col-print-12 details"><h4>Piro Farm</h4>
+    <a href="https://pirofarm-blog.tumblr.com/" target="_blank" class="link">
+    https://pirofarm-blog.tumblr.com/</a></div>
+      <div class="col-xs-12 col-print-12"><p>Experiments in gardening and maker on activities.</p>
+
+      </div>
+    </div>
+
+
+  </div>
+
+
+
+
+  <div class="container education-container">
+    <h2>Education</h2>
+
+      <div class="row clearfix layout layout-left">
+    <div class="col-xs-12 col-sm-4 col-md-3 col-print-12 details"><h4>Carnegie Mellon University</h4><p><b>BS Electrical and Computer Engineering</b></p><p>1999 - 2003</p></div>
+    <div class="col-xs-12 col-sm-8 col-md-9 col-print-12"><p>I focused on the interface between hardware and software.</p>
+  <ul>
+    <li>Course work: Operating Systems, Embedded Systems, Advanced Digital Design and Advanced Prototyping Lab</li>
+    <li>Teaching Assistant: Embedded Systems and Advanced Digital Design</li>
+    <li>Languages: C, Verilog, Java</li>
+    <li>Tools: XEmacs, Make, CVS</li>
+  </ul>
+
+    </div>
+  </div>
+
+
+  </div>
+  <div class="container more-container">
+    <h2>Certifications</h2>
+    <div class="row clearfix">
+      <div class="col-md-12">
+        <p>Amazon Web Services</p>
+  <ul>
+    <li><a href="https://www.certmetrics.com/amazon/public/badge.aspx?i=4&amp;t=c&amp;d=2019-12-20&amp;ci=AWS00532053">AWS Solutions Architect Professional</a></li>
+    <li>AWS Devops Professional</li>
+    <li>AWS Networking Specialist</li>
+    <li>AWS Security Specialist</li>
+    <li>AWS Big Data Specialist</li>
+    <li>AWS Machine Learning Specialist</li>
+    <li>AWS Solutions Architect Associate</li>
+    <li>AWS Sysops Associate</li>
+    <li>AWS Developer Associate</li>
+  </ul>
+
+      </div>
+    </div>
+  </div>
+  </div>
+  </main>
+
+  <div class="container footer-container">
+    <p>Kam Lasater - <a href="mailto:ckl@seekayel.com" target="_blank">ckl@seekayel.com</a></p>
+  </div>
+  <script src="/assets/js/index.js"></script>
 </body>
