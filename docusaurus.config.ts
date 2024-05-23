80637a6db3cc5d92b2fc4ad63e3bbe3457798b72
Kam Lasater <ckl@seekayel.com>
2024-05-22 21:03:05 -0400

    setup for gh-pages

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -3,7 +3,7 @@ import type {Config} from '@docusaurus/types';
 import type * as Preset from '@docusaurus/preset-classic';
 
 const config: Config = {
-  title: 'My Site',
+  title: 'Kam Lasater - Personal site',
   tagline: 'Dinosaurs are cool',
   favicon: 'img/favicon.ico',
 
@@ -15,8 +15,8 @@ const config: Config = {
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'seekayel', // Usually your GitHub org/user name.
+  projectName: 'kamlasater.com', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
