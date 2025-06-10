16b7868399b565c8941ae5fd7be18bd5cff9d7dd
Kam Lasater <ckl@seekayel.com>
2025-06-10 09:20:49 -0400

    tweak header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -13,19 +13,19 @@ export default function Index() {
         </li>
         </ul>
         <ul class="menu-right">
-        <li>
-            <a target="_blank" href="https://github.com/seekayel">
-            <i class="fa-brands fa-github" title="Github link"></i>
-            </a>
-        </li><li>
-            <a target="_blank" href="https://www.linkedin.com/in/kamlasater">
-            <i class="fa-brands fa-linkedin" title="Linkedin link"></i>
-            </a>
-        </li><li>
-            <a target="_blank" href="https://twitter.com/seekayel">
-            <i class="fa-brands fa-twitter" title="Twitter link"></i>
-            </a>
-        </li>
+            <li>
+                <a target="_blank" href="https://github.com/seekayel">
+                <i class="fa-brands fa-github" title="Github link"></i>
+                </a>
+            </li><li>
+                <a target="_blank" href="https://www.linkedin.com/in/kamlasater">
+                <i class="fa-brands fa-linkedin" title="Linkedin link"></i>
+                </a>
+            </li><li>
+                <a target="_blank" href="https://twitter.com/seekayel">
+                <i class="fa-brands fa-twitter" title="Twitter link"></i>
+                </a>
+            </li>
         </ul>
     </nav>
     </header>
